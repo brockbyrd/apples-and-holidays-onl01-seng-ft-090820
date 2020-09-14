f5e0a88ae887358ea615206a5415b227a69435f8
Brock Byrd <brockbyrdd@gmail.com>
2020-09-14 20:31:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :spring
       holiday.each do |day, supply|
-        supply << "Grill" + "Table Clothe"
+        supply << "Grill" + "Table Cloth"
       end
     end
   end
